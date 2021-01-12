6ae4e38ad79567def0b8c828fe58d3497745205f
Sebastian Joseph <theclimax25@gmail.com>
2021-01-12 08:26:03 -0500

    introduced simple command for response

---
+++ NarutoBot.py
@@ -0,0 +1,21 @@
+import discord
+import random
+import time
+import asyncio
+
+client = discord.Client()
+TOKEN = "Nzk4NDI5MzA4NTg2MDMzMTgz.X_05PQ.WHKB1U_O0RZdQ4Iourk-soXZZzg"
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith("Naruto!"):
+
+        if str(message.author) == "Seb#3204":
+            await message.channel.send("Hello Master!")
+        else:
+            await message.channel.send("Believe It!")
+
+client.run(TOKEN)
